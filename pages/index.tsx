1d391a0deca091860f1616434795db4db98f0c56
Brad <moonirbootchamir@gmail.com>
2023-01-30 13:49:21 +0100

    slide badges

--- pages/index.tsx
+++ pages/index.tsx
@@ -197,7 +197,7 @@ export default function Home() {
        </section>
 
        <section>
-       <div className='w-1/3 h-[750px]  m-auto py-16 px-4 relative group'>
+       <div className='max-w-[550px] h-[650px]  m-auto py-16 px-4 relative group'>
       <div
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
         className='w-full h-full rounded-2xl  bg-center duration-500'
