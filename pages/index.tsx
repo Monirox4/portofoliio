521006fe885b311f735d9fed2a263ddf5a16add6
Brad <moonirbootchamir@gmail.com>
2023-01-30 13:19:46 +0100

    slide badges

--- pages/index.tsx
+++ pages/index.tsx
@@ -197,10 +197,10 @@ export default function Home() {
        </section>
 
        <section>
-       <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
+       <div className='w-1/3 h-[780px]  m-auto py-16 px-4 relative group'>
       <div
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-        className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+        className='w-full h-full rounded-2xl bg-center bg-scroll duration-500'
       ></div>
       {/* Left Arrow */}
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
